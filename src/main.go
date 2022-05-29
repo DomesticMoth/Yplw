632f6b20c3cda51a187fac3a215efd22d01d12e2
DomesticMoth <silkmoth@protonmail.com>
2022-05-29 21:11:48 +0000

    Add resolver function

--- src/main.go
+++ src/main.go
@@ -91,29 +91,45 @@ func normaliseUris(peers []string) []string {
 	return ret
 }
 
+func resolveNames(peers []string) []string {
+	ret := []string{}
+	for _, peer := range peers {
+		ret = append(ret, peer)
+		u, err := url.Parse(peer)
+		if err != nil { continue }
+		host, port, _ := net.SplitHostPort(u.Host)
+		if host == ""{ continue }
+		addr := net.ParseIP(host)
+		if addr != nil { continue }
+		ips ,err := net.LookupIP(host)
+		if err != nil { continue }
+		if u.Scheme == "tls" {
+			// add sni
+			q, _ := url.ParseQuery(u.RawQuery)
+			q.Add("sni", host)
+			u.RawQuery = q.Encode()
+		}
+		for _, ip := range ips {
+			if ip.To4() == nil {
+				// v6
+				u.Host = "["+ip.String()+"]:"+port
+				ret = append(ret, u.String())
+			}else{
+				//v4
+				u.Host = ip.String()+":"+port
+				ret = append(ret, u.String())
+			}
+		}
+	}
+	return ret
+}
+
 func main() {
 	peers, err := getPeersList()
     if err != nil { panic(err) }
     peers = normaliseUris(peers)
+    peers = resolveNames(peers)
     for _, peer := range peers {
     	fmt.Println(peer)
     }
 }
-
-
-/*
-socks://localhost:4447/7mx6ztmimo5nrnmydjjtkr6maupknr3zlyr33umly22pqnivyxcq.b32.i2p:46944
-socks://localhost:4447/gqt6l2wox5jndysfllrgdr6mp473t24mdi7f3iz6lugpzv3z67wq.b32.i2p:63412
-socks://localhost:4447/i6lbsjw7kh4gqmbylcsjtfh3juj3dbbk24bwzrpgvtalhs7xagoa.b32.i2p:2721
-socks://localhost:4447/2ddoxwucodjfy6u34v3toxm2a463nzd3f523hhfzc3y5de7twj6a.b32.i2p:2281
-socks://localhost:4447/ro7bwwx7ch6echfqwgivsi37tgojgcrjosgq5jrgx5ebadu4xsaq.b32.i2p:39565
-socks://localhost:4447/5cejostqhllvdnbjgmtsua46z6wt5eiecirljvthlz45yvn5hswa.b32.i2p:61944
-socks://localhost:4447/xmgzqfidm3zn4y3vfljqiuyfxorifntjca3rfnwv3dbbfcrx4uca.b32.i2p:30112
-socks://localhost:4447/3qqi3lxscvx2ebatj36y6wmdzaah7eblf5fl7scayp6wgyhh3vpa.b32.i2p
-socks://localhost:9050/yggnekkmyitzepgl5ltdl277y5wdg36n4pc45sualo3yesm3usnuwyad.onion:1863
-socks://localhost:9050/x7dqdmjb7y5ykj4kgirwzj62wrrd3t5dv57oy7oyidnf7cpthd4k7ryd.onion:5222
-socks://localhost:9050/nxuwjikhsirri2rbrdlphstsn3jr2qzjrsylwkt65rh2miycr5n24tid.onion:706
-socks://localhost:9050/fllrj72kxnenalmmi3uz22ljqnmuex4h2jlhwnapxlzrnn7lknadxuqd.onion:706
-socks://localhost:9050/douchedeiqqvyyylqorwpej4q3oz46n2shpngp7d27tlcnufnpwag7ad.onion:5222
-socks://localhost:9050/p2pkbqdgvabddixbbr2y7vrra4qxq3sejfep2qknfu4owh7e3i622dqd.onion:133
-*/
